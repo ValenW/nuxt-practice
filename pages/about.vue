4efff57763896007763b0e50de95634978a179a5
Valen <wang_wenl@worksap.co.jp>
2020-08-11 16:12:17 +0800

    init nuxt

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <h1>About</h1>
+</div>
+</template>
+
+<script>
+export default {
+ name: 'about',
+  data() {
+    return {
+
+    }
+  },
+  components: {},
+  watch: {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <h1>Home</h1>
+</div>
+</template>
+
+<script>
+export default {
+ name: 'index',
+  data() {
+    return {
+
+    }
+  },
+  components: {},
+  watch: {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
